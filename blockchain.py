b1116f28f27c7c572c0c6b75774a5972ef426cfe
Matthew Lam <minlam@microsoft.com>
2021-02-28 13:37:48 -0800

    update return doc

--- blockchain.py
+++ blockchain.py
@@ -10,90 +10,110 @@ class Blockchain(object):
         # Create the genesis block with no previous block
         self.newBlock(prevHash = 1, proof = 100)
 
-        def newBlock(self, proof, prevHash = None):
-            """
-            Creates a new block and adds it to the blockchain
+    def newBlock(self, proof, prevHash = None):
+        """
+        Creates a new block and adds it to the blockchain
 
-            Args:
-                proof (int): Proof of work given by algo
-                prevHash (str, optional): Hash of previous block. Defaults to None.
-            """
-            
-            block = {
-                "index": len(self.chain) + 1,
-                "timestamp": time(),
-                "transactions": self.curTransactions,
-                "proof": proof,
-                "prevHash": prevHash or self.hash(self.chain[-1],)
-            }
+        Args:
+            proof (int): Proof of work given by algo
+            prevHash (str, optional): Hash of previous block. Defaults to None.
 
-            # Reset current transactions
-            self.curTransactions = []
+        Returns:
+            dict: New block
+        """
+        
+        block = {
+            "index": len(self.chain) + 1,
+            "timestamp": time(),
+            "transactions": self.curTransactions,
+            "proof": proof,
+            "prevHash": prevHash or self.hash(self.chain[-1],)
+        }
 
-            self.chain.append(block)
-            return block
+        # Reset current transactions
+        self.curTransactions = []
 
-        def newTransaction(self, sender, recipient, amount):
-            """
-            Adds new transaction to list of current transactions to be mined in next Block
+        self.chain.append(block)
+        return block
 
-            Args:
-                sender (str): Address of the Sender
-                recipient (str): Address of the Recipient
-                amount (int): Amount of transaction
-            """
-            
-            self.curTransactions.append(
-                {"sender": sender,
-                "recipient": recipient,
-                "amount": amount,}
-            )
+    def newTransaction(self, sender, recipient, amount):
+        """
+        Adds new transaction to list of current transactions to be mined in next Block
 
-            return self.lastBlock("index") + 1
+        Args:
+            sender (str): Address of the Sender
+            recipient (str): Address of the Recipient
+            amount (int): Amount of transaction
 
-        @property
-        def lastBlock(self):
-            return self.chain[-1]
+        Returns:
+            int: Index of the block that will hold this transaction
+        """
+        
+        self.curTransactions.append(
+            {"sender": sender,
+            "recipient": recipient,
+            "amount": amount,}
+        )
 
-        @staticmethod
-        def hash(block):
-            """
-            Creates a SHA-256 hash for a block
+        return self.lastBlock("index") + 1
 
-            Args:
-                block (dict): Block
-            """
+    @property
+    def lastBlock(self):
+        """
+        Returns the last block of the chain
 
-            # The dictionary representing block must be ordered, or we'll have inconsistent hashing
-            blockStr = json.dumps(block, sort_keys=True).encode()
-            return hashlib.sha256(blockStr).hexdigest()
+        Returns:
+            dict: Last block
+        """
+        return self.chain[-1]
 
-        def proofWork(self, lastProof):
-            """
-            Proof of work Algorithm:
-                - Find a numper p' such that hash(pp') contains 4 leading 0's, where p is previous p' (proof of prevBlock)
-                - p is the previous proof, and p' is the new proof
+    @staticmethod
+    def hash(block):
+        """
+        Creates a SHA-256 hash for a block
 
-            Args:
-                lastProof (int): Last proof (proof of prevBlock)
-            """
+        Args:
+            block (dict): Block
 
-            proof = 0
-            while self.validProof(lastProof, proof) is False:
-                proof += 1
+        Returns:
+            str: hash of the block
+        """
 
-            return proof
+        # The dictionary representing block must be ordered, or we'll have inconsistent hashing
+        blockStr = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(blockStr).hexdigest()
 
-        def validProof(self, lastProof, proof):
-            """
-            Validates the proof: Does hash(lastProof, proof) have 4 leading 0's?
+    def proofWork(self, lastProof):
+        """
+        Proof of work Algorithm:
+            - Find a numper p' such that hash(pp') contains 4 leading 0's, where p is previous p' (proof of prevBlock)
+            - p is the previous proof, and p' is the new proof
 
-            Args:
-                lastProof (int): last proof
-                proof (int): current proof
-            """
+        Args:
+            lastProof (int): Last proof (proof of prevBlock)
 
-            guess = f'{lastProof}{proof}'.encode()
-            guessHash = hashlib.sha256(guess).hexdigest()
-            return guessHash[:4] == "0000"
+        Returns:
+            int
+        """
 
+        proof = 0
+        while self.validProof(lastProof, proof) is False:
+            proof += 1
+
+        return proof
+
+    def validProof(self, lastProof, proof):
+        """
+        Validates the proof: Does hash(lastProof, proof) have 4 leading 0's?
+
+        Args:
+            lastProof (int): last proof
+            proof (int): current proof
+
+        Returns:
+            bool: Whether the pass proof is valid with prevProof
+        """
+
+        guess = f'{lastProof}{proof}'.encode()
+        guessHash = hashlib.sha256(guess).hexdigest()
+        return guessHash[:4] == "0000"
